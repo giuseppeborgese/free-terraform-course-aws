cbbe103e26eaa4403be92e9e17851b1c47746e95
Giuseppe Borgese <giuseppe.borgese.ext@siemens.com>
2021-11-18 11:38:03 +0100

    example of data

---
+++ init/data.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "last_ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]  # Canonical
+  name_regex = "^ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+}
+
+output "latest_ubuntu" {
+  value = data.aws_ami.last_ubuntu.image_id
+}
