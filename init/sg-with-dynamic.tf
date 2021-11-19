9f1a299289c4f3feae2bfd84495438e390b3e42f
Giuseppe Borgese <giuseppe.borgese.ext@siemens.com>
2021-11-19 16:06:50 +0100

    dynamic blocks

---
+++ init/sg-with-dynamic.tf
@@ -0,0 +1,49 @@
+variable "rules" {
+  type = list(object({
+    port        = number
+    proto       = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      port        = 80
+      proto       = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      port        = 22
+      proto       = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      port        = 3689
+      proto       = "tcp"
+      cidr_blocks = ["6.7.8.9/32"]
+    }
+  ]
+
+}
+resource "aws_security_group" "my-sg" {
+  name = "my-dynamic-sg"
+
+  dynamic "ingress" {
+    for_each = var.rules
+    content {
+      from_port   = ingress.value["port"]
+      to_port     = ingress.value["port"]
+      protocol    = ingress.value["proto"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terraform-Dynamic-SG"
+  }
+}
